79246e37791de2b90c005143b8a343b407d99afa
CARLITOS <CARLITOS@DESKTOP-DN009DA>
2021-03-17 11:53:46 +0100

    Empieza las operaciones

---
+++ src/Pruebas/Prueba1.java
@@ -0,0 +1,23 @@
+package Pruebas;
+
+import java.util.Scanner;
+
+public class Prueba1 {
+
+	public static void main(String[] args) {
+		
+		Scanner ent = new Scanner(System.in);
+		//hacer una suma y resta
+		System.out.println("Introduce dos numero para la operacion");
+		int a = 0, b = 0;
+		a = ent.nextInt();
+		b= ent.nextInt();
+		int total = a+b;
+		
+		
+		System.out.println(total);
+		//haz la resta aqui
+
+	}
+
+}
